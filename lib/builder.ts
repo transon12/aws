ffb546e632470a62e55f0c6c726c0400dda5c13f
Sontx <sontx@vnext.com.vn>
2021-12-16 13:27:02 +0700

    code build

--- lib/builder.ts
+++ lib/builder.ts
@@ -1,23 +1,62 @@
 import * as cdk from '@aws-cdk/core';
 import ec2 = require('@aws-cdk/aws-ec2');
 import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as iam from '@aws-cdk/aws-iam';
 
 export class builder extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+        // Create ACM Permission Policy
+        const describeAcmCertificates = new iam.PolicyDocument({
+            statements: [
+                new iam.PolicyStatement({
+                    resources: ['*'],
+                    actions: [
+                        "sts:AssumeRole",
+                        "cloudformation:*",
+                        "ec2:*",
+                        "ssm:GetParameters",
+                        "iam:*",
+                        "autoscaling:*",
+                        "elasticloadbalancing:*",
+                        "cloudwatch:*"
+                    ],
+                }),
+            ],
+        });
+
+        const codeBuildRole = new iam.Role(this, 'example-iam-role', {
+            assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com'),
+            description: 'An example IAM role in AWS CDK',
+            inlinePolicies: {
+                DescribeACMCerts: describeAcmCertificates,
+            },
+            managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName(
+                    'AmazonAPIGatewayInvokeFullAccess',
+                ),
+            ],
+        });
+
         new codebuild.Project(this, 'MyProject', {
-            source: codebuild.Source.bitBucket({
-                owner: 'awslabs',
-                repo: 'aws-cdk',
-            }),
             buildSpec: codebuild.BuildSpec.fromObject({
                 version: '0.2',
+                environment: {
+                    'computeType': codebuild.ComputeType.SMALL,
+                },
+                role: codeBuildRole,
                 phases: {
                     build: {
                         commands: [
+                            "eho start build",
+                            "export VERSION=$(date +\\%Y\\%m\\%d\\%H\\%M\\%S)",
+                            "npm install -g aws-cdk@1.133",
+                            "npm install -g typescript",
+                            "cdk --version",
                             "git clone https://github.com/transon12/aws.git",
+                            "cd aws",
                             "npm install",
-                            "cdk deploy VpcCreateNewStack",
+                            "cdk deploy VpcCreateNewStack  --require-approval never",
                             "export EC2_INSTANCE_ID=$(ec2metadata --instance-id)",
                             "echo EC2_INSTANCE_ID"
                         ],
--- lib/vpc-create-new-stack.ts
+++ lib/vpc-create-new-stack.ts
@@ -9,7 +9,7 @@ export class VpcCreateNewStack extends cdk.Stack {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, "Vpc", {
-      maxAzs: 3,
+      maxAzs: 2,
       natGateways: 0,
       subnetConfiguration: [
         { name: 'public', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC },
@@ -24,17 +24,14 @@ export class VpcCreateNewStack extends cdk.Stack {
     webserverSG.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(22),
-      'allow SSH access from anywhere',
     );
     webserverSG.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(80),
-      'allow HTTP traffic from anywhere',
     );
     webserverSG.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(443),
-      'allow HTTPS traffic from anywhere',
     );
 
     //  create a Role for the EC2 Instance
